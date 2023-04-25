027c57ad606adc8cb0fa8ec7d8f2674089ee98b4
geomonitora@gmail.com <geomonitora@gmail.com>
2023-04-25 19:55:07 -0300

    Aula 4

--- src/paginas/Post/index.jsx
+++ src/paginas/Post/index.jsx
@@ -1,10 +1,29 @@
+import "./Post.css";
 import React from "react";
 import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 export default function Post() {
   const parametros = useParams();
 
-  console.log(parametros);
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
 
-  return <h1>Post {parametros.id}</h1>;
+  if (!post) {
+    return <h1>Post não encontrado...</h1>;
+  }
+
+  return (
+    <PostModelo
+      fotoCapa={`/assets/posts/${post.id}/capa.png`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>{post.texto}</ReactMarkdown>
+      </div>
+    </PostModelo>
+  );
 }
